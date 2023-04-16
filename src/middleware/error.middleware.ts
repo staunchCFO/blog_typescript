36197daa922fb933fc42e2000aad8ca5a800c04f
Emeka Okezie <okezieemeka949@gmail.com>
2023-04-16 17:14:03 +0100

    Added validation

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import HttpException from '@/utils/exceptions/http.exception';
 
-function errorMiddleware(req: Request, res: Response, error: HttpException, next: NextFunction): void {
+function errorMiddleware(error: HttpException, req: Request, res: Response, _next: NextFunction): void {
     const status = error.status || 500;
     const message = error.message || 'Something went wrong';
 
